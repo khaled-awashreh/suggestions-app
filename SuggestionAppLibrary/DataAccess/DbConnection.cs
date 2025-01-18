17eb12c9cf25b86df84814e99d4276e5cd663b12
Khaled Awashreh <khaled.awashreh@foothillsolutions.com>
2025-01-18 20:47:27 +0200

    setup db connection, secrets, atlas connection string, db configurations

--- SuggestionAppLibrary/DataAccess/DbConnection.cs
+++ SuggestionAppLibrary/DataAccess/DbConnection.cs
@@ -1,6 +1,36 @@
+using Microsoft.Extensions.Configuration;
+using MongoDB.Driver;
+
 namespace SuggestionAppLibrary.DataAccess;
 
 public class DbConnection
 {
+    private readonly IConfiguration _configuration;
+    private readonly IMongoDatabase _database;
+    private string _connectionId = "MongoDB";
     
+    public string DbName { get; private set; }
+
+    public string CategoryCollectionName  { get; private set; } = "categories";
+    public string StatusCollectionName  { get; private set; } = "statuses";
+    public string UserCollectionName  { get; private set; } = "users";
+    public string SuggestionCollectionName  { get; private set; } = "suggestions";
+    
+    public MongoClient Client { get; private set; }
+    public IMongoCollection<Category> CategoryCollection { get; private set; }
+    public IMongoCollection<Suggestion> SuggestionCollection { get; private set; }
+    public IMongoCollection<User> UserCollection { get; private set; }
+    public IMongoCollection<SuggestionStatus> StatusCollection { get; private set; }
+    public DbConnection(IConfiguration configuration)
+    {
+        _configuration = configuration;
+        Client = new MongoClient(_configuration.GetConnectionString(_connectionId));
+        DbName = _configuration["DatabaseName"];
+        _database = Client.GetDatabase(DbName);
+        
+        CategoryCollection = _database.GetCollection<Category>(CategoryCollectionName);
+        SuggestionCollection = _database.GetCollection<Suggestion>(SuggestionCollectionName);
+        UserCollection = _database.GetCollection<User>(UserCollectionName);
+        StatusCollection = _database.GetCollection<SuggestionStatus>(StatusCollectionName);
+    }
 }
